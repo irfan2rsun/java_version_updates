920990a90fbf1d76be2b805a39374d2e935c70ce
Irfan Tursun <irfan2rsun@gmail.com>
2024-02-21 07:50:22 -0800

    generics

---
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,24 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList <Integer> items = new ArrayList<Integer>();//<generics>Arrays list only accepts objects belongs to int
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        //items.add("Apple")
+
+        printDoubled(items);
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items){
+        for (int i : items){
+            System.out.println(i*2);
+        }
+    }
+}
